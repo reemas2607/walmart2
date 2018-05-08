21bb39e60dcb73902929aa29959d88015c792d9b
reemas2607 <39100292+reemas2607@users.noreply.github.com>
2018-05-09 00:29:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.walmart.com/ip/International-Concepts-Mission-Unfinished-Entertainment-TV-Stand-for-TVs-up-to-52/42420884");
 die();
 ?>
